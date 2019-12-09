e414803564eadc3b0898140917bd10a8356fbaac
jinju2828 <jinju2828@gmail.com>
2019-12-09 14:15:59 -0800

    animatio

--- main.js
+++ main.js
@@ -8,4 +8,47 @@ let body = selectElement('body');
 
 menuToggler.addEventListener('click', function () {
     body.classList.toggle('open');
-})
+});
+
+// Scroll reveal
+window.sr = ScrollReveal();
+
+sr.reveal('.animate-left', {
+    origin: 'left',
+//    where the animation is coming from
+    duration: 1000,
+//    ms
+    distance: '25rem',
+//    how far it's travelling from
+    delay: 300
+});
+
+sr.reveal('.animate-right', {
+    origin: 'right',
+//    where the animation is coming from
+    duration: 1000,
+//    ms
+    distance: '25rem',
+//    how far it's travelling from
+    delay: 600
+});
+
+sr.reveal('.animate-top', {
+    origin: 'top',
+//    where the animation is coming from
+    duration: 1000,
+//    ms
+    distance: '25rem',
+//    how far it's travelling from
+    delay: 600
+});
+
+sr.reveal('.animate-bottom', {
+    origin: 'bottom',
+//    where the animation is coming from
+    duration: 1000,
+//    ms
+    distance: '25rem',
+//    how far it's travelling from
+    delay: 600
+});
